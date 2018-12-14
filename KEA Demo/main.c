e327552ec85b8b5a591f8ee7df72fc91efb5d8d2
无风引漩 <zyf199966@126.com>
2018-12-14 21:42:49 +0800

    30s外侧
    Co-Authored-By: highlevelwallace <highlevelwallace@users.noreply.github.com>
    Co-Authored-By: skycraft01 <skycraft01@users.noreply.github.com>

--- KEA Demo/main.c
+++ KEA Demo/main.c
@@ -4,14 +4,15 @@ uint8 data_getstring[2];
 uint16_t AD1 = 0, AD2 = 0;
 uint16_t count;
 float pre_offset = 0, offset = 0;
-
+const int speed=130;
+const float mid=500.0;
 void Control()
 {
   //补充你的控制代码
 
   //读取AD值
   AD1 = ADC_Read(ADC0_SE1);
-  AD2 = ADC_Read(ADC0_SE2);
+  AD2 = ADC_Read(ADC0_SE3);
 
   //舵机控制 建议使用位置式PD控制 请参考相应的手册
   pre_offset = offset;
@@ -21,13 +22,21 @@ void Control()
   FTM_Count_Clean(ftm1);       //编码器数值清零
 
   //电机控制，建议对电机与舵机的占空比限幅，电机0~100%，舵机根据安装情况设置
-  FTM_PWM_Duty(ftm2,ftm_ch0,100);
+  FTM_PWM_Duty(ftm2,ftm_ch0,400);
 }
 
 void PIT_Interrupt(uint8 ch)
 {
   GPIO_Turn(G2);
   GPIO_Turn(G3);
+}
+
+void OLED_Myshow(void)
+{
+  OLED_Clear(0x00);
+  sprintf(spring_oled, "LL:%d", 1234); //
+  OLED_Show_String(8, 16, 0, 0, 1, spring_oled, 0);
+  OLED_Refresh_Gram();
 }
 
 int main(void)
@@ -45,42 +54,37 @@ int main(void)
   Disable_Interrupt(INT_PIT_CH0);
   Enable_Interrupt(INT_PIT_CH0);
   FTM_PWM_Init(ftm0, ftm_ch0, A0, 300, 500);   //舵机
-  FTM_PWM_Init(ftm2, ftm_ch1, F1, 14000, 100); //电机
+  FTM_PWM_Init(ftm2, ftm_ch1, F1, 14000, 110); //电机
 
   //ADC
-  ADC_Init(ADC0_SE1, ADC_12bit);  //A1
-  ADC_Init(ADC0_SE2, ADC_12bit);  //A6
-  ADC_Init(ADC0_SE3, ADC_12bit);  //A7
-  ADC_Init(ADC0_SE9, ADC_12bit);  //C1
-  ADC_Init(ADC0_SE10, ADC_12bit); //C2
+  ADC_Init(ADC0_SE1, ADC_12bit);  //A1,AD1
+  ADC_Init(ADC0_SE2, ADC_12bit);  //A6,AD3
+  ADC_Init(ADC0_SE3, ADC_12bit);  //A7,AD2
+  ADC_Init(ADC0_SE9, ADC_12bit);  //C1,AD4
+  ADC_Init(ADC0_SE10, ADC_12bit); //C2,AD5
+
 
  //OLED部分书写（不确定） 
     OLED_Init();
+    
     OLED_Display_Config(1);
     while(1){
        OLED_Clear(0x00);
        AD1 = ADC_Read(ADC0_SE1);
        AD2 = ADC_Read(ADC0_SE3);
-       OLED_Show_String(8,16,0,20,1,(char*)AD1,0);
-       OLED_Show_String(8,16,80,20,1,(char*)AD2,0);
+       sprintf(spring_oled, "%d", AD1);
+       OLED_Show_String(8,16,0,20,1,spring_oled,0);
+       sprintf(spring_oled, "%d", AD2);
+       OLED_Show_String(8,16,80,20,1,spring_oled,0);
        OLED_Refresh_Gram();
-       //在0行0列显示AD1
-       //里面的AD1要求是char,不知道强制转换能不能转化为char
-      offset = (float)100*(AD1 - AD2)/(AD1 + AD2);
-      FTM_PWM_Duty(ftm0, ftm_ch0, 500+offset);
-    }
- //电机部分书写（不确定）
-    while(1){
-      AD1 = ADC_Read(ADC0_SE1);
-      AD2 = ADC_Read(ADC0_SE3);
-      if(AD1-AD2>30){
-        offset = (float)1000*(AD1 - AD2)/(AD1 + AD2);
-        FTM_PWM_Duty(ftm0, ftm_ch0, 500+offset);
-        FTM_PWM_Duty(ftm2, ftm_ch1, 80-fabs(offset));
+       offset = (float)100*(AD1 - AD2)/(AD1 + AD2 + 10);
+      if(fabs(offset)>40){//转弯的offset阈值
+        FTM_PWM_Duty(ftm0, ftm_ch0, (int)(mid-offset*2));//乘数为转弯系数
+        FTM_PWM_Duty(ftm2, ftm_ch1, speed-fabs(offset)/3.6);//除数为减速系数
       }
-      else {
-        FTM_PWM_Duty(ftm0, ftm_ch0, 500);
-        FTM_PWM_Duty(ftm2, ftm_ch1, 100);
+      else {//直行
+        FTM_PWM_Duty(ftm0, ftm_ch0, mid);
+        FTM_PWM_Duty(ftm2, ftm_ch1, speed);
       }
     }
-}
+    }
