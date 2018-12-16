2ee62cbbd58509937b6347156c7e70b04d6f3243
无风引漩 <zyf199966@126.com>
2018-12-16 18:32:20 +0800

    Update main.c

--- KEA Demo/main.c
+++ KEA Demo/main.c
@@ -14,6 +14,7 @@ uint16_t count;
 float pre_offset = 0, offset = 0;
 const int speed = 132;
 const float mid = 530.0;
+
 void Control()
 {
 	//补充你的控制代码
@@ -85,37 +86,17 @@ int main(void)
 	ADC_Init(ADC0_SE3, ADC_12bit);  //A7,AD2
 	ADC_Init(ADC0_SE9, ADC_12bit);  //C1,AD4
 	ADC_Init(ADC0_SE10, ADC_12bit); //C2,AD5
-
-   //OLED部分书写（不确定） 
+        
+        //OLED
 	OLED_Init();
-
 	OLED_Display_Config(1);
-	while (1) {
+        
+	while (1)
+        {
 		OLED_Clear(0x00);
 		AD1 = ADC_Read(ADC0_SE1);
 		AD2 = ADC_Read(ADC0_SE3);
-		//sprintf(spring_oled, "%d", AD1);
-		//OLED_Show_String(8,16,0,20,1,spring_oled,0);
-		//sprintf(spring_oled, "%d", AD2);
-		//OLED_Show_String(8,16,80,20,1,spring_oled,0);
-		//OLED_Refresh_Gram();
 		offset = (float)100 * (AD1 - AD2) / (AD1 + AD2 + 10);
-		/*const int straight_adjust_thres = 10, turn_thres = 45;
-		if (fabs(offset) > straight_adjust_thres && fabs(offset) <= turn_thres) {//直道调整
-			servoturn((offset > 0 ? 1 : -1)*(fabs(offset) - straight_adjust_thres)*0.7);//乘数为转弯系数
-			motorspeed(speed);
-		}
-		else if (fabs(offset) > turn_thres) {//转弯的offset阈值
-			servoturn(offset*2.6);//乘数为转弯系数
-			if (fabs(offset) > turn_thres + 20)
-				motorspeed(fabs(offset) / 4.8);//除数为减速系数
-			else
-				motorspeed(speed - fabs(offset) / 2.7);//除数为减速系数
-		}
-		else {//直行
-			FTM_PWM_Duty(ftm0, ftm_ch0, mid);
-			FTM_PWM_Duty(ftm2, ftm_ch1, speed);
-		}*///以上是原来的控制代码
 		servoturn((offset > 0 ? 1 : -1) * turnconvert(fabs(offset)));//乘数为转弯系数
 		motorspeed(speed - 0.1 * turnconvert(fabs(offset)));//在offset<24时不减速
 		sprintf(spring_oled, "%.2f", offset);
