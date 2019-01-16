b528f2643f9a8ecf8026a7b8c8bc4001ce766264
无风引漩 <zyf199966@126.com>
2019-01-16 21:08:06 +0800

    完成了起跑线检测算法，但需要硬件配合：下拉电阻和干簧管

--- KEA Demo/main.c
+++ KEA Demo/main.c
@@ -12,6 +12,8 @@ int main(void)
     MYInit();
     int nowspeed = speed;
     int inRing = 0;
+    int lap = 0;         // 干簧管控制的 圈数计数器
+    int isStartLine = 0; // 圈首标识
 
     while (1)
     {
@@ -22,18 +24,39 @@ int main(void)
         AD2 = ADC_Read(ADC0_SE3);
         AD3 = ADC_Read(ADC0_SE2);
         AD4 = ADC_Read(ADC0_SE9);
-        sprintf(spring_oled, "%3d %3d %3d %3d", AD1, AD2, AD3, AD4);
+        sprintf(spring_oled, "%3d%3d%3d%3d", AD1, AD2, AD3, AD4);
         OLED_Show_String(8, 16, 0, 0, 1, spring_oled, 0);
-
-        //count = FTM_Pulse_Get(ftm1); //编码器数值读取
-        //FTM_Count_Clean(ftm1); //编码器数值清零
-
+        sprintf(spring_oled, "offset:%.2f", offset);
+        OLED_Show_String(8, 16, 0, 32, 1, spring_oled, 0);
+        sprintf(spring_oled, "I0:%d", Pin(I0));
+        OLED_Show_String(8, 16, 0, 16, 1, spring_oled, 0);
+        /*
+        //编码器部分：请注意，编码器应选用脉冲-方向型，且须接在speed2
+        count = FTM_Pulse_Get(ftm1);
+        sprintf(spring_oled, "%d", count);
+        OLED_Show_String(8, 16, 0, 48, 1, spring_oled, 0);
+        FTM_Count_Clean(ftm1);
+        */
         // 使用PD设置偏移量
         offset = (float)100 * (AD1 - AD4) / (AD1 + AD4 + 10);
         const int straight_adjust_thres = 30, turn_thres = 45;
 
-        if (inRing)
+        if (Pin(I0)&& Pin(H1) && Pin(H0)) // 发车模块
         {
+            isStartLine++;
+        }
+        else if (isStartLine > 3) // 若连续检测到起跑线，则认定为起跑线
+        {
+            lap++;
+            isStartLine = 0;
+        }
+
+        if (lap > 3) // 若跑完三圈，则停车
+        {
+            setMotor(-100);
+            Soft_Delay_ms(200);
+            setMotor(0);
+            return 0;
         }
 
         if (AD1 + AD4 <= 20 && nowspeed) // 出赛道自动停车，赛时需要移除
@@ -42,14 +65,12 @@ int main(void)
             nowspeed = 0;
             setMotor(0);
         }
-        else 
-	    if(fabs(offset)>straight_adjust_thres&&fabs(offset)<=turn_thres)//直道调整
-	    {
-      	    setSteer((int)(mid-(offset>0?1:-1)*(fabs(offset)-straight_adjust_thres)*0.7));//乘数为转弯系数
-    	    //FTM_PWM_Duty(ftm2, ftm_ch1, speed);//除数为减速系数
-    	}
-    	else
-            if (fabs(offset) > turn_thres)
+        else if (fabs(offset) > straight_adjust_thres && fabs(offset) <= turn_thres) //直道调整
+        {
+            setSteer((int)(mid - (offset > 0 ? 1 : -1) * (fabs(offset) - straight_adjust_thres) * 0.7)); //乘数为转弯系数
+            //FTM_PWM_Duty(ftm2, ftm_ch1, speed);//除数为减速系数
+        }
+        else if (fabs(offset) > turn_thres)
         {                                        //转弯的offset阈值
             setSteer((int)(mid - offset * 2.6)); //乘数为转弯系数
             if (fabs(offset) > turn_thres + 20)
@@ -62,8 +83,6 @@ int main(void)
             setMotor(speed);
             setSteer(mid);
         }
-        sprintf(spring_oled, "%.2f", offset);
-        OLED_Show_String(8, 16, 0, 32, 1, spring_oled, 0);
 
         OLED_Refresh_Gram();
     }
--- KEA Demo/main.h
+++ KEA Demo/main.h
@@ -1,6 +1,5 @@
 #include "common.h"
 
-#define setMotor(speed) FTM_PWM_Duty(ftm2, ftm_ch1, speed)
 #define setSteer(dir) FTM_PWM_Duty(ftm0, ftm_ch0, dir)
 
 char spring_oled[20];
@@ -11,13 +10,31 @@ float pre_offset = 0, offset = 0;
 const int speed = 132;
 const float mid = 520.0;
 
+void setMotor(int s)
+{
+    if (s > 0)
+    {
+        FTM_PWM_Duty(ftm2, ftm_ch1, s < speed ? s : speed); // 设置了速度上限：speed
+    }
+    else if (!s)
+    {
+        FTM_PWM_Duty(ftm2, ftm_ch1, 0);
+        FTM_PWM_Duty(ftm2, ftm_ch0, 0);
+    }
+    else
+    {
+        FTM_PWM_Duty(ftm2, ftm_ch0, -s < speed ? -s : speed); // 设置了速度下限：-speed
+    }
+}
+
 void MYInit()
 {
     while ((1280 * ex_clk_khz) != (256 * ics_clk_khz))
         ; //确保时钟配置无误
 
-    FTM_PWM_Init(ftm0, ftm_ch0, A0, 300, mid);   //舵机
+    FTM_PWM_Init(ftm0, ftm_ch0, A0, 300, mid); //舵机
     FTM_PWM_Init(ftm2, ftm_ch1, F1, 14000, 0); //电机
+    FTM_PWM_Init(ftm2, ftm_ch0, F0, 14000, 0); //电机倒转
 
     //ADC
     ADC_Init(ADC0_SE1, ADC_12bit);  //A1,AD1
@@ -26,7 +43,21 @@ void MYInit()
     ADC_Init(ADC0_SE9, ADC_12bit);  //C1,AD4
     ADC_Init(ADC0_SE10, ADC_12bit); //C2,AD5
 
+    //编码器
+    FTM_Pulse_Init(ftm1, FTM_PS_1, TCLK2);
+
     //OLED
     OLED_Init();
     OLED_Display_Config(1);
+
+    //干簧管
+    GPIO_Init(I0, GPI, LOW); //IO1
+    GPIO_Init(H1, GPI, LOW); //IO2
+    GPIO_Init(H0, GPI, LOW); //IO3
+
+    //蜂鸣器
+    GPIO_Init(I1, GPO, LOW);
+
+    //LED
+    GPIO_Init(G1, GPO, LOW);
 }
