fa6dcd5f0687e35bf2431aac81cc3c04498e878e
无风引漩 <zyf199966@126.com>
2019-01-16 17:44:28 +0800

    添加了两个传感器

--- KEA Demo/main.c
+++ KEA Demo/main.c
@@ -10,45 +10,60 @@ void PIT_Interrupt(uint8 ch)
 int main(void)
 {
     MYInit();
+    int nowspeed = speed;
+    int inRing = 0;
 
     while (1)
     {
         // 读取AD值并打印
         OLED_Clear(0x00);
         AD1 = ADC_Read(ADC0_SE1);
+        AD1 = AD1 > 15 ? AD1 - 15 : (AD1 > 7 ? AD1 - 7 : AD1); // 对AD值的偏差做了调整！！
         AD2 = ADC_Read(ADC0_SE3);
-        sprintf(spring_oled, "%d", AD1);
-        OLED_Show_String(8, 16, 0, 20, 1, spring_oled, 0);
-        sprintf(spring_oled, "%d", AD2);
-        OLED_Show_String(8, 16, 80, 20, 1, spring_oled, 0);
+        AD3 = ADC_Read(ADC0_SE2);
+        AD4 = ADC_Read(ADC0_SE9);
+        sprintf(spring_oled, "%3d %3d %3d %3d", AD1, AD2, AD3, AD4);
+        OLED_Show_String(8, 16, 0, 0, 1, spring_oled, 0);
+
+        //count = FTM_Pulse_Get(ftm1); //编码器数值读取
+        //FTM_Count_Clean(ftm1); //编码器数值清零
 
         // 使用PD设置偏移量
-        offset = (float)100 * (AD1 - AD2) / (AD1 + AD2 + 10);
+        offset = (float)100 * (AD1 - AD4) / (AD1 + AD4 + 10);
         const int straight_adjust_thres = 30, turn_thres = 45;
 
-        /*
+        if (inRing)
+        {
+        }
+
+        if (AD1 + AD4 <= 20 && nowspeed) // 出赛道自动停车，赛时需要移除
+        {
+            Soft_Delay_ms(500);
+            nowspeed = 0;
+            setMotor(0);
+        }
+        else 
 	    if(fabs(offset)>straight_adjust_thres&&fabs(offset)<=turn_thres)//直道调整
 	    {
-      	    FTM_PWM_Duty(ftm0, ftm_ch0, (int)(mid-(offset>0?1:-1)*(fabs(offset)-straight_adjust_thres)*0.7));//乘数为转弯系数
-    	    FTM_PWM_Duty(ftm2, ftm_ch1, speed);//除数为减速系数
+      	    setSteer((int)(mid-(offset>0?1:-1)*(fabs(offset)-straight_adjust_thres)*0.7));//乘数为转弯系数
+    	    //FTM_PWM_Duty(ftm2, ftm_ch1, speed);//除数为减速系数
     	}
-    	else*/
-
-        if (fabs(offset) > turn_thres)
-        {                                                           //转弯的offset阈值
-            FTM_PWM_Duty(ftm0, ftm_ch0, (int)(mid - offset * 2.6)); //乘数为转弯系数
+    	else
+            if (fabs(offset) > turn_thres)
+        {                                        //转弯的offset阈值
+            setSteer((int)(mid - offset * 2.6)); //乘数为转弯系数
             if (fabs(offset) > turn_thres + 20)
-                FTM_PWM_Duty(ftm2, ftm_ch1, speed - fabs(offset) / 4.8); //除数为减速系数
+                setMotor(speed - fabs(offset) / 4.8); //除数为减速系数
             else
-                FTM_PWM_Duty(ftm2, ftm_ch1, speed - fabs(offset) / 2.7); //除数为减速系数
+                setMotor(speed - fabs(offset) / 2.7); //除数为减速系数
         }
         else
         { //直行
             setMotor(speed);
             setSteer(mid);
         }
-        //sprintf(spring_oled, "%.2f", offset);
-        //OLED_Show_String(8,16,0,20,1,spring_oled,0);
+        sprintf(spring_oled, "%.2f", offset);
+        OLED_Show_String(8, 16, 0, 32, 1, spring_oled, 0);
 
         OLED_Refresh_Gram();
     }
--- KEA Demo/main.h
+++ KEA Demo/main.h
@@ -5,32 +5,24 @@
 
 char spring_oled[20];
 uint8 data_getstring[2];
-uint16_t AD1 = 0, AD2 = 0;
+uint16_t AD1 = 0, AD2 = 0, AD3 = 0, AD4 = 0;
 uint16_t count;
 float pre_offset = 0, offset = 0;
 const int speed = 132;
-const float mid = 530.0;
-
-
-/*
-void setMotor(uint32 duty)
-{
-    
-}
-*/
+const float mid = 520.0;
 
 void MYInit()
 {
     while ((1280 * ex_clk_khz) != (256 * ics_clk_khz))
         ; //确保时钟配置无误
 
-    FTM_PWM_Init(ftm0, ftm_ch0, A0, 300, 500);   //舵机
-    FTM_PWM_Init(ftm2, ftm_ch1, F1, 14000, 110); //电机
+    FTM_PWM_Init(ftm0, ftm_ch0, A0, 300, mid);   //舵机
+    FTM_PWM_Init(ftm2, ftm_ch1, F1, 14000, 0); //电机
 
     //ADC
     ADC_Init(ADC0_SE1, ADC_12bit);  //A1,AD1
-    ADC_Init(ADC0_SE2, ADC_12bit);  //A6,AD3
     ADC_Init(ADC0_SE3, ADC_12bit);  //A7,AD2
+    ADC_Init(ADC0_SE2, ADC_12bit);  //A6,AD3
     ADC_Init(ADC0_SE9, ADC_12bit);  //C1,AD4
     ADC_Init(ADC0_SE10, ADC_12bit); //C2,AD5
 
