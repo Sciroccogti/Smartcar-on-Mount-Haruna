515d1f75379135ca60d76ec5e61bba4e8a979eae
无风引漩 <zyf199966@126.com>
2019-02-21 20:41:05 +0800

    完成代码优化，更改AD对应

--- KEA Demo/main.c
+++ KEA Demo/main.c
@@ -1,3 +1,5 @@
+// 比完赛晚上秋名山见 
+// Last updated: 2-21-2019 By 张逸帆
 /****************************************
 2018年12月16日 朱雨齐
 调整缩进格式
@@ -5,38 +7,12 @@
 然后写了一个新的转向舵机函数模型
 y=e^(a*x^2+cx+b)-e^b
 *****************************************/
-#include "common.h"
+#include "main.h"
 #include "math.h"
-char spring_oled[20];
-uint8 data_getstring[2];
-uint16_t AD1 = 0, AD2 = 0;
-uint16_t count;
-float pre_offset = 0, offset = 0;
-const int speed = 132;
-const float mid = 530.0;
-
-void Control()
-{
-	//补充你的控制代码
-
-	//读取AD值
-	AD1 = ADC_Read(ADC0_SE1);
-	AD2 = ADC_Read(ADC0_SE3);
-
-	//舵机控制 建议使用位置式PD控制 请参考相应的手册
-	pre_offset = offset;
-	offset = (float)1000 * (AD1 - AD2) * 1.0 / (AD1 + AD2 + 1);
-
-	count = FTM_Pulse_Get(ftm1); //编码器数值读取
-	FTM_Count_Clean(ftm1);       //编码器数值清零
-
-	//电机控制，建议对电机与舵机的占空比限幅，电机0~100%，舵机根据安装情况设置
-	FTM_PWM_Duty(ftm2, ftm_ch0, 400);
-}
 
 void servoturn(int offset)//转向
 {
-	FTM_PWM_Duty(ftm0, ftm_ch0, mid - offset);
+	SetSteer(kMidSteer - offset);
 }
 
 double turnconvert(double x)//offset与舵机转向的转换函数
@@ -45,60 +21,23 @@ double turnconvert(double x)//offset与舵机转向的转换函数
 	return exp(a * x * x + c * x + b) - exp(b);
 }
 
-void motorspeed(int sp)//“油”门
-{
-	FTM_PWM_Duty(ftm2, ftm_ch1, sp);
-}
-
 void PIT_Interrupt(uint8 ch)
 {
 	GPIO_Turn(G2);
 	GPIO_Turn(G3);
 }
 
-void OLED_Myshow(void)
-{
-	OLED_Clear(0x00);
-	sprintf(spring_oled, "LL:%d", 1234); //
-	OLED_Show_String(8, 16, 0, 0, 1, spring_oled, 0);
-	OLED_Refresh_Gram();
-}
-
 int main(void)
 {
-	while ((1280 * ex_clk_khz) != (256 * ics_clk_khz)); //确保时钟配置无误
-	GPIO_Init(G1, GPO, LOW);
-	Soft_Delay_ms(1000);
-	GPIO_Turn(G1);
-	GPIO_Init(G2, GPO, LOW);
-	GPIO_Init(G3, GPO, HIGH);
-
-	PIT_Init1(pit0, 100000);
-	PIT_SetCallback(PIT_Interrupt);
-	Disable_Interrupt(INT_PIT_CH0);
-	Enable_Interrupt(INT_PIT_CH0);
-	FTM_PWM_Init(ftm0, ftm_ch0, A0, 300, 500);   //舵机
-	FTM_PWM_Init(ftm2, ftm_ch1, F1, 14000, 110); //电机
-
-	//ADC
-	ADC_Init(ADC0_SE1, ADC_12bit);  //A1,AD1
-	ADC_Init(ADC0_SE2, ADC_12bit);  //A6,AD3
-	ADC_Init(ADC0_SE3, ADC_12bit);  //A7,AD2
-	ADC_Init(ADC0_SE9, ADC_12bit);  //C1,AD4
-	ADC_Init(ADC0_SE10, ADC_12bit); //C2,AD5
-        
-        //OLED
-	OLED_Init();
-	OLED_Display_Config(1);
-        
+	MYInit();
 	while (1)
         {
 		OLED_Clear(0x00);
 		AD1 = ADC_Read(ADC0_SE1);
-		AD2 = ADC_Read(ADC0_SE3);
-		offset = (float)100 * (AD1 - AD2) / (AD1 + AD2 + 10);
+		AD4 = ADC_Read(ADC0_SE9);
+		offset = (float)100 * (AD1 - AD4) / (AD1 + AD4 + 10);
 		servoturn((offset > 0 ? 1 : -1) * turnconvert(fabs(offset)));//乘数为转弯系数
-		motorspeed(speed - 0.1 * turnconvert(fabs(offset)));//在offset<24时不减速
+		SetMotor(kTopSpeed - 0.1 * turnconvert(fabs(offset)));//在offset<24时不减速
 		sprintf(spring_oled, "%.2f", offset);
 		OLED_Show_String(8, 16, 0, 20, 1, spring_oled, 0);
 		OLED_Refresh_Gram();
---
+++ KEA Demo/main.h
@@ -0,0 +1,70 @@
+// 比完赛晚上秋名山见 
+// Last updated: 2-21-2019 By 张逸帆
+// 命名规范参见https://zh-google-styleguide.readthedocs.io/en/latest/google-cpp-styleguide/naming/#
+// const常量请以 k 开头，大小写混合
+// 函数请以大写开头，大小写混合
+
+#include "common.h"
+
+#define SetSteer(dir) FTM_PWM_Duty(ftm0, ftm_ch0, dir)
+
+char spring_oled[20];
+uint8 data_getstring[2];
+uint16_t AD1 = 0, AD2 = 0, AD3 = 0, AD4 = 0;
+uint16_t count;
+float pre_offset = 0, offset = 0;
+const int kTopSpeed = 132; //  速度上限
+const float kMidSteer = 520.0;
+const int kTotalLap = 1; //  圈数（资格赛）
+
+void SetMotor(int s) // 支持直接设置负数
+{
+    if (s > 0)
+    {
+        FTM_PWM_Duty(ftm2, ftm_ch1, s < kTopSpeed ? s : kTopSpeed); // 设置了速度上限：kTopSpeed
+    }
+    else if (!s)
+    {
+        FTM_PWM_Duty(ftm2, ftm_ch1, 0);
+        FTM_PWM_Duty(ftm2, ftm_ch0, 0);
+    }
+    else
+    {
+        FTM_PWM_Duty(ftm2, ftm_ch0, -s < kTopSpeed ? -s : kTopSpeed); // 设置了速度下限：-kTopSpeed
+    }
+}
+
+void MYInit()
+{
+    while ((1280 * ex_clk_khz) != (256 * ics_clk_khz))
+        ; //确保时钟配置无误
+
+    FTM_PWM_Init(ftm0, ftm_ch0, A0, 300, kMidSteer); //舵机
+    FTM_PWM_Init(ftm2, ftm_ch1, F1, 14000, 0); //电机
+    FTM_PWM_Init(ftm2, ftm_ch0, F0, 14000, 0); //电机倒转
+
+    //ADC
+    ADC_Init(ADC0_SE1, ADC_12bit);  //A1,AD1
+    ADC_Init(ADC0_SE3, ADC_12bit);  //A7,AD2
+    ADC_Init(ADC0_SE2, ADC_12bit);  //A6,AD3
+    ADC_Init(ADC0_SE9, ADC_12bit);  //C1,AD4
+    ADC_Init(ADC0_SE10, ADC_12bit); //C2,AD5
+
+    //编码器
+    FTM_Pulse_Init(ftm1, FTM_PS_1, TCLK2);
+
+    //OLED
+    OLED_Init();
+    OLED_Display_Config(1);
+
+    //干簧管
+    GPIO_Init(I0, GPI, LOW); //IO1
+    GPIO_Init(H1, GPI, LOW); //IO2
+    GPIO_Init(H0, GPI, LOW); //IO3
+
+    //蜂鸣器
+    GPIO_Init(I1, GPO, LOW);
+
+    //LED
+    GPIO_Init(G1, GPO, LOW);
+}
